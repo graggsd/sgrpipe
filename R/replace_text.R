f027f4a743cde941969f49c717f354d5c0a0d39a
Stephen Gragg <graggsd@uab.edu>
2018-01-01 15:34:49 -0600

    Finished writing function to replace text

--- R/replace_text.R
+++ R/replace_text.R
@@ -18,12 +18,24 @@ replace_regex_text <- function (x, from, to) {
     return(x)
 }
 
+search_text <- function(x, from) {
+    return(sum(sapply(from, grepl, x)) > 0)
+}
+
+#' @export
 replace_text <- function(dir, from, to) {
+    # Stop if from and to lengths don't match
     stopifnot(length(from) == length(to) | length(to) == 1)
+    # Get all .R .Rmd and README file names
     files <- get_file_names(dir)
     for(file in files) {
+        # For each file, read line by line
         tx <- readLines(paste0(dir, file))
-        tx2 <- replace_regex_text(tx, from, to)
-        writeLines(tx2, paste0(dir, file))
+        # Re-write file only if one of the regexs in the from argument is
+        # contained within a given file
+        if (search_text(tx, from)) {
+            tx2 <- replace_regex_text(tx, from, to)
+            writeLines(tx2, paste0(dir, file))
+        }
     }
 }
