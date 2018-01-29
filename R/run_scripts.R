e1420dba98deaea4e38c2bc49f729e4a4bcdb884
Stephen Gragg <graggsd@uab.edu>
2018-01-29 16:09:41 -0600

    Updated pipeline functions to have one universal command for launching an R script.

--- R/run_scripts.R
+++ R/run_scripts.R
@@ -1,14 +1,12 @@
-#' @export
-run_R <- function(x) {
+run_dotR <- function(x) {
     source(x)
     rm_globals()
 }
 
-#' @export
-run_Rmd <- function(x,
-                    out_dir = "../results",
-                    output_format = "html_document",
-                    ...) {
+run_dotRmd <- function(x,
+                       out_dir = "../results",
+                       output_format = "html_document",
+                       ...) {
     if (output_format == "html_document") {
         output_file <- paste0(out_dir, "/", sub("\\.Rmd$", ".html", x))
     }
@@ -18,6 +16,23 @@ run_Rmd <- function(x,
                       output_format = output_format,
                       ...)
     rm_globals()
+}
+
+#' @export
+run_R <- function(x,
+                  out_dir = "../results",
+                  output_format = "html_document",
+                  ...) {
+
+    if (grepl("\\.R$", x)) {
+        run_dotR(x = x)
+    } else if (grepl("\\.Rmd", x)) {
+
+        run_dotRmd(x = x,
+                   out_dir = out_dir,
+                   output_format = output_format,
+                   ...)
+    }
 
 }
 
@@ -26,3 +41,4 @@ rm_globals <- function() {
     x <- ls(pos = ".GlobalEnv")
     rm(list = x, pos = ".GlobalEnv")
 }
+
