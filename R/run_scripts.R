e0d620e2d6b42a234faed83f3b7ce3bf40d36d35
Stephen Gragg <graggsd@uab.edu>
2018-01-30 08:13:28 -0600

    Added documentation to run_R

--- R/run_scripts.R
+++ R/run_scripts.R
@@ -18,6 +18,30 @@ run_dotRmd <- function(x,
     rm_globals()
 }
 
+# Helper function to clean environment
+rm_globals <- function() {
+    x <- ls(pos = ".GlobalEnv")
+    rm(list = x, pos = ".GlobalEnv")
+}
+
+#' Launches scripts from \code{.R} or \code{.Rmd} files
+#'
+#' Depending on the type of file, the function will source code from a \code{.R}
+#' file or render a document from a \code{.Rmd}.
+#'
+#' @param x name of file
+#' @param out_dir location in which to store documents compiled from \code{.Rmd}
+#' files
+#' @param output_format format in which to compile a \code{.Rmd} document
+#' @param ... additional parameters to pass to \code{rmarkdown::render} when
+#' rendering a \code{.Rmd} file
+#' @return NULL
+#'
+#' This function is meant to facilitate launching a series of preprocessing,
+#' data exploration, and analysis scripts in R. After each script is run,
+#' any existing global variables are removed; therefore, data passed from one
+#' script to another should be outputed as an intermediate .RData file.
+#'
 #' @export
 run_R <- function(x,
                   out_dir = "../results",
@@ -34,11 +58,5 @@ run_R <- function(x,
                    ...)
     }
 
-}
-
-# Helper function to clean environment
-rm_globals <- function() {
-    x <- ls(pos = ".GlobalEnv")
-    rm(list = x, pos = ".GlobalEnv")
 }
 
